d989a05913de3dedcfa8f6d4653d0c6d55e23757
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2023-06-16 15:23:06 -0400

    fix: only appbundle gets debug path in PRs

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,23 +8,19 @@ class BuildPath {
   final String androidAPK;
 
   factory BuildPath() {
-    if (Platform.environment['GITHUB_EVENT_NAME'] == 'pull_request') {
-      return BuildPath._(
-        linux: 'build/linux/x64/debug/bundle',
-        windows: r'build\windows\runner\Debug',
-        macos: 'build/macos/Build/Products/Debug',
-        androidBundle: 'build/app/outputs/bundle/debug',
-        androidAPK: 'build/app/outputs/flutter-apk',
-      );
-    } else {
-      return BuildPath._(
-        linux: 'build/linux/x64/release/bundle',
-        windows: r'build\windows\runner\Release',
-        macos: 'build/macos/Build/Products/Release',
-        androidBundle: 'build/app/outputs/bundle/release',
-        androidAPK: 'build/app/outputs/flutter-apk',
-      );
-    }
+    final bool isPullRequest =
+        Platform.environment['GITHUB_EVENT_NAME'] == 'pull_request';
+
+    final appBundleDebugPath = 'build/app/outputs/bundle/debug';
+    final appBundleReleasePath = 'build/app/outputs/bundle/release';
+
+    return BuildPath._(
+      linux: 'build/linux/x64/release/bundle',
+      windows: r'build\windows\runner\Release',
+      macos: 'build/macos/Build/Products/Release',
+      androidBundle: isPullRequest ? appBundleDebugPath : appBundleReleasePath,
+      androidAPK: 'build/app/outputs/flutter-apk',
+    );
   }
 
   BuildPath._({
