a70df0728d9d7d8fd528553ff7d88be01a789a38
Kristen McWilliam <9575627+Merrit@users.noreply.github.com>
2024-03-08 12:00:40 -0500

    don't exit for subcommand stderr output
    Many subcommands print to stderr even though they are successful, so
    we don't want to exit on stderr output.

--- lib/terminal.dart
+++ lib/terminal.dart
@@ -23,34 +23,11 @@ abstract class Terminal {
     final result = await Process.run(executable, arguments);
 
     if (result.stderr != '') {
-      log.e('\n${result.stderr}');
-      await printPubLog();
-      exit(1);
+      // We warn, but don't exit because many dependant commands output to
+      // stderr, but still succeed.
+      log.w('\n${result.stderr}');
     }
 
     return result.stdout;
   }
-}
-
-/// Print the pub log if there is any.
-///
-/// If there is a log, it should be located at `~/.pub-cache/log/pub_log.txt`.
-Future<void> printPubLog() async {
-  log.i('Printing pub log');
-
-  final String? home = Platform.environment['HOME'];
-  if (home == null) {
-    log.e('HOME environment variable is not set');
-    return;
-  }
-
-  final pubLog = File('$home/.pub-cache/log/pub_log.txt');
-  final exists = await pubLog.exists();
-
-  if (!exists) {
-    log.i('pub log does not exist');
-    return;
-  }
-
-  log.e('\n\npub log:\n${pubLog.readAsStringSync()}');
 }
